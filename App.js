1e9cc95e1c2af58ec88f8b7e8e7a9f8bfdc305b1
chnoor999 <onlyforpc709@gmail.com>
2024-01-30 22:54:11 +0500

    cleaning

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StyleSheet } from "react-native";
-import React from "react";
 //screens
 import GameStartScreen from "./app/screens/GameStartScreen";
 import Screen from "./app/screens/Screen";
